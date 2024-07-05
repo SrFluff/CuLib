f964cc2510c37193b5a2e07c2eca1fb54a9984f6
Franco <168871500+SrFluff@users.noreply.github.com>
2024-07-05 19:11:12 -0400

    Add CuLib.py

---
+++ CuLib.py
@@ -0,0 +1,37 @@
+# All imports MUST go here ⬇️
+
+import os
+import random
+import time
+
+# CuLib
+#
+# Version 1.0.0 'Journey's end'
+#
+# Made by Franco M.
+
+def cls():
+
+    # I keep adding this to my projects :3
+
+    if os.name == 'nt': # Checks if the OS kerenel is NT(windows)
+
+        os.system('cls') # Windows cmd command
+
+    else: # If it's not NT then it's posix
+
+        os.system('clear') # Not a Windows cmd command
+
+def random10():
+
+    # I've needed this before, somehow
+
+    print(random.randint(0,10))
+
+        # I love randint <3
+
+def wait(wai):
+
+    #zzz zzz zzz zzz zzz zzz zzz
+
+    time.sleep(wai)
